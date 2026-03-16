02cb44fff6846a219ade1d1ae2ddd179a81f2008
enzo012345 <figueroa.enzo@escola.pr.gov.br>
2026-03-16 11:43:44 -0300

    Add files via upload

---
+++ aula 1 instrucaoeatribuicao.py
@@ -0,0 +1,27 @@
+#Variavel é um espaço de memória que tem um nome e guarda algum dado do meu programa
+nome = "Enzo"
+sobrenome = "Nathan"
+numero = 1
+print(nome+sobrenome)
+print(numero)
+
+resultado = numero + numero + 5
+print(resultado)
+
+#Expressoes sao quaisquer combinações de valor, variável e operadores
+resultado = numero + numero + 5
+resultados = (numero * (numero * numero) + 33)
+
+#A instrução print mostra o programa no terminal
+print(numero)
+
+
+# Precedencia de operadores
+# Parenteses precendencia mais alta
+# Exponenciação
+# multiplicação e a Divisão
+# Adição e a Subtração
+
+# Concatenação de Strings
+nomeInteiro = nome + sobrenome
+print(nomeInteiro)
